a33345463acaec2b1ab58c6b7c5d0af2d5d7a8f3
natkalia <natalia.banasiewicz@wp.pl>
2019-11-07 12:05:57 +0100

    Move object with questions list to separate script

---
+++ questions.js
@@ -0,0 +1,35 @@
+const questionsList = {
+  questionItem1: {
+    questionNumber: 1,
+    questionText: 'In all low-income countries across the world today, how many girls finish primary school?',
+    questionImage: '',
+    answers: {
+      a: '20%',
+      b: '40%',
+      c: '60%'
+    },
+    correct: 'c'
+  },
+  questionItem2: {
+    questionNumber: 2,
+    questionText: 'Where does the majority of the world population live?',
+    questionImage: '',
+    answers: {
+      a: 'Low-income countries',
+      b: 'Middle-income countries',
+      c: 'High-income countries'
+    },
+    correct: 'b'
+  },
+  questionItem3: {
+    questionNumber: 3,
+    questionText: 'In the last 20 years the proportion of the world population living in extreme poverty has?',
+    questionImage: '',
+    answers: {
+      a: 'Almost doubled',
+      b: 'Remained more or less the same',
+      c: 'Almost halved'
+    },
+    correct: 'c'
+  }
+};
