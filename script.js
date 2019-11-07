d2147ca33195dc88653c249da2e4e9ac75086038
natkalia <natalia.banasiewicz@wp.pl>
2019-11-07 13:34:20 +0100

    Refactor function to build quiz from object, add auto numbers

--- script.js
+++ script.js
@@ -1,19 +1,29 @@
 const quizBox = document.querySelector(".quiz-inner");
 
 function quizBuilder() {
+  let num = 0;
   for (item in questionsList) {
     const { questionText } = questionsList[item];
     const { questionImage } = questionsList[item];
+    const { answers } = questionsList[item];
 
+    //creating question with number based on object
     const questionBox = document.createElement("p");
     quizBox.appendChild(questionBox);
-    questionBox.innerText = questionText;
+    function autoNum(x) {
+      num = ++num;
+      console.log(num, x);
+      x = `${num}. ${x}`;
+      return x;
+    }
+    questionBox.innerText = autoNum(questionText);
 
+    //creating image based on object
     const imageBox = document.createElement("img");
     quizBox.appendChild(imageBox);
     imageBox.src = questionImage;
-
-    const { answers } = questionsList[item];
+    
+    //creating answers based on object
     const answersBoxList = document.createElement("ol");
     quizBox.appendChild(answersBoxList);
 
