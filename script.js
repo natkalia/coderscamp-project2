7b539e4b614afe14438c35bc6d6777396e7da072
natkalia <natalia.banasiewicz@wp.pl>
2019-11-07 12:58:17 +0100

    Add more code create quiz from object

--- script.js
+++ script.js
@@ -1,20 +1,29 @@
-const quizBox = document.querySelector('.quiz-inner');
+const quizBox = document.querySelector(".quiz-inner");
 
 function quizBuilder() {
-  
   for (item in questionsList) {
     const { questionText } = questionsList[item];
     const { questionImage } = questionsList[item];
 
-    const questionBox = document.createElement('div');
+    const questionBox = document.createElement("p");
     quizBox.appendChild(questionBox);
     questionBox.innerText = questionText;
 
-    const imageBox = document.createElement('img');
+    const imageBox = document.createElement("img");
     quizBox.appendChild(imageBox);
     imageBox.src = questionImage;
-  }
 
+    const { answers } = questionsList[item];
+
+    for (letter in answers) {
+      answer = answers[letter];
+      const answersBoxList = document.createElement("ul");
+      const answersBoxListItem = document.createElement("li");
+      quizBox.appendChild(answersBoxList);
+      answersBoxList.appendChild(answersBoxListItem);
+      answersBoxList.innerText = answer;
+    }
+  }
 }
 
 quizBuilder();
