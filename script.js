f187705bbeee6a5b3b33dc14893404c1a6761b48
natkalia <natalia.banasiewicz@wp.pl>
2019-11-07 12:38:30 +0100

    Add initial code to dynamically create quiz from object

--- script.js
+++ script.js
@@ -1,35 +1,20 @@
-const questionsList = {
-  questionItem1: {
-    questionNumber: 1,
-    questionText: 'In all low-income countries across the world today, how many girls finish primary school?',
-    questionImage: '',
-    answers: {
-      a: '20%',
-      b: '40%',
-      c: '60%'
-    },
-    correct: 'c'
-  },
-  questionItem2: {
-    questionNumber: 2,
-    questionText: 'Where does the majority of the world population live?',
-    questionImage: '',
-    answers: {
-      a: 'Low-income countries',
-      b: 'Middle-income countries',
-      c: 'High-income countries'
-    },
-    correct: 'b'
-  },
-  questionItem3: {
-    questionNumber: 3,
-    questionText: 'In the last 20 years the proportion of the world population living in extreme poverty has?',
-    questionImage: '',
-    answers: {
-      a: 'Almost doubled',
-      b: 'Remained more or less the same',
-      c: 'Almost halved'
-    },
-    correct: 'c'
+const quizBox = document.querySelector('.quiz-inner');
+
+function quizBuilder() {
+  
+  for (item in questionsList) {
+    const { questionText } = questionsList[item];
+    const { questionImage } = questionsList[item];
+
+    const questionBox = document.createElement('div');
+    quizBox.appendChild(questionBox);
+    questionBox.innerText = questionText;
+
+    const imageBox = document.createElement('img');
+    quizBox.appendChild(imageBox);
+    imageBox.src = questionImage;
   }
-};
+
+}
+
+quizBuilder();
