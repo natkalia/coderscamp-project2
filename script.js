80b3ba0339969f4a868221ccdd5c8d7d6bbd8e27
natkalia <natalia.banasiewicz@wp.pl>
2019-11-07 10:11:07 +0100

    Add initial quiz questions to js object

---
+++ script.js
@@ -0,0 +1,33 @@
+const questions = [
+  {
+    question: 'In all low-income countries across the world today, how many girls finish primary school?',
+    options: {
+      a: '20%',
+      b: '40%',
+      c: '60%'
+    },
+    answer: 'c'
+  },
+  {
+    question: 'Where does the majority of the world population live?',
+    options: {
+      a: 'Low-income countries',
+      b: 'Middle-income countries',
+      c: 'High-income countries'
+    },
+    answer: 'b'
+  },
+  {
+    question: 'In the last 20 years the proportion of the world population living in extreme poverty has?',
+    options: {
+      a: 'Almost doubled',
+      b: 'Remained more or less the same',
+      c: 'Almost halved'
+    },
+    answer: 'c'
+  }
+];
+
+
+
+
