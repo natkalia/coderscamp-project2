852c1b790eeb00041f4aeef2b447e7823979afa8
natkalia <natalia.banasiewicz@wp.pl>
2019-11-13 09:55:47 +0100

    Add more questions to quiz and links to images

--- questions.js
+++ questions.js
@@ -1,32 +1,146 @@
 const questionsList = [
   {
-    questionText: 'In all low-income countries across the world today, how many girls finish primary school?',
-    questionImage: 'assets/image-school.jpg',
+    questionText:
+      "In all low-income countries across the world today, how many girls finish primary school?",
+    questionImage: "assets/girls-school.jpg",
     answersToQuestion: {
-      a: '20%',
-      b: '40%',
-      c: '60%'
+      a: "20%",
+      b: "40%",
+      c: "60%"
     },
-    correct: 'c'
+    correct: "c"
   },
   {
-    questionText: 'Where does the majority of the world population live?',
-    questionImage: 'assets/population.jpg',
+    questionText: "Where does the majority of the world population live?",
+    questionImage: "assets/income-level.jpg",
     answersToQuestion: {
-      a: 'Low-income countries',
-      b: 'Middle-income countries',
-      c: 'High-income countries'
+      a: "Low-income countries",
+      b: "Middle-income countries",
+      c: "High-income countries"
     },
-    correct: 'b'
+    correct: "b"
   },
   {
-    questionText: 'In the last 20 years the proportion of the world population living in extreme poverty has?',
-    questionImage: 'assets/poverty.jpg',
+    questionText:
+      "In the last 20 years the proportion of the world population living in extreme poverty has?",
+    questionImage: "assets/poverty-population.jpg",
     answersToQuestion: {
-      a: 'Almost doubled',
-      b: 'Remained more or less the same',
-      c: 'Almost halved'
+      a: "Almost doubled",
+      b: "Remained more or less the same",
+      c: "Almost halved"
     },
-    correct: 'c'
+    correct: "c"
+  },
+  {
+    questionText: "What is the life expectancy of the world today?",
+    questionImage: "assets/life-expectancy.jpg",
+    answersToQuestion: {
+      a: "50 years",
+      b: "60 years",
+      c: "70 years"
+    },
+    correct: "c"
+  },
+  {
+    questionText:
+      "There are 2 billion children in the world today, aged 0 to 15 years old. How many children will there be in the year 2100, according to the United Nations?",
+    questionImage: "assets/children-number.jpg",
+    answersToQuestion: {
+      a: "4 billion",
+      b: "3 billion",
+      c: "2 billion"
+    },
+    correct: "c"
+  },
+  {
+    questionText:
+      "The UN predicts that by 2100 the world population will have increased by another 4 billion people. What is the main reason?",
+    questionImage: "assets/increase-population.jpg",
+    answersToQuestion: {
+      a: "There will be more children aged below 15",
+      b: "There will be more adults (age 15 to 74)",
+      c: "There will be more very old people (age 75 and older)"
+    },
+    correct: "b"
+  },
+  {
+    questionText:
+      "How did the number of deaths per year from natural disasters change over the last hundred years?",
+    questionImage: "assets/disaster-deaths.jpg",
+    answersToQuestion: {
+      a: "More than doubled",
+      b: "Remained about the same",
+      c: "Decreased to less than half"
+    },
+    correct: "c"
+  },
+  {
+    questionText:
+      "There are roughly 7 billion people in the world today, which option most accurately represents where they live?",
+    questionImage: "assets/population-continents.jpg",
+    answersToQuestion: {
+      a:
+        "1 billion in Europe, 4 billion in Asia, 1 billion in Africa, and 1 billion in the Americas",
+      b:
+        "1 billion in Europe, 3 billion in Asia, 2 billion in Africa, and 1 billion in the Americas",
+      c:
+        "1 billion in Europe, 3 billion in Asia, 1 billion in Africa, and 2 billion in the Americas"
+    },
+    correct: "a"
+  },
+  {
+    questionText:
+      "How many of the world’s 1-year-old children today have been vaccinated against some disease?",
+    questionImage: "assets/vaccination.jpg",
+    answersToQuestion: {
+      a: "20%",
+      b: "50%",
+      c: "80%"
+    },
+    correct: "c"
+  },
+  {
+    questionText:
+      "Worldwide, 30-year-old men have spent 10 years in school, on average. How many years have women of the same age spent in school?",
+    questionImage: "assets/women-school.jpg",
+    answersToQuestion: {
+      a: "9 years",
+      b: "6 years",
+      c: "3 years"
+    },
+    correct: "a"
+  },
+  {
+    questionText:
+      "In 1996, tigers, giant pandas, and black rhinos were all listed as endangered. How many of these three species are more critically endangered today?",
+    questionImage: "assets/animals-danger.jpg",
+    answersToQuestion: {
+      a: "Two of them",
+      b: "One of them",
+      c: "None of them"
+    },
+    correct: "c"
+  },
+  {
+    questionText:
+      "How many people in the world have SOME access to electricity?",
+    questionImage: "assets/electricity.jpg",
+    answersToQuestion: {
+      a: "20%",
+      b: "50%",
+      c: "80%"
+    },
+    correct: "c"
+  },
+  {
+    questionText:
+      "Global climate experts believe that, over the next 100 years, the average temperature will...",
+    questionImage: "assets/climate-change.jpg",
+    answersToQuestion: {
+      a: "get warmer",
+      b: "remain the same",
+      c: "get colder"
+    },
+    correct: "a"
   }
 ];
